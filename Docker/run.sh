8632111bf0ff83980da86a03fd228c438a87af43
Andrey Tikhonov <andrey.tikhonov.1974@gmail.com>
2020-11-24 18:11:07 +0200

    Extended field size

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo docker run -d  --name yahoo_parser  --net=host -e DBHOST='localhost' -e DBPORT=3306 -e DATABASE='traxessag' -e DBUSER='admin' -e DBPASSWORD='Qwerty@123' -v /home/android/DataOx:/work parser:yahoo  *
+sudo docker run -d  --name yahoo_parser  --net=host -e DBHOST='localhost' -e DBPORT=3306 -e DATABASE='traxessag' -e DBUSER='admin' -e DBPASSWORD='Qwerty@123' -v /home/android/DataOx:/work parser:yahoo  $1 $2
 
