cd8305a320385de7641d68302fc9e120616f682a
Andrey Tikhonov <andrey.tikhonov.1974@gmail.com>
2020-11-24 18:25:11 +0200

    Fixed error if emtpy list of companies was returned

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+docker stop yahoo_parser
+docker rm yahoo_parser
 sudo docker run -d  --name yahoo_parser  --net=host -e DBHOST='localhost' -e DBPORT=3306 -e DATABASE='traxessag' -e DBUSER='admin' -e DBPASSWORD='Qwerty@123' -v /home/android/DataOx:/work parser:yahoo  $1 $2
 
