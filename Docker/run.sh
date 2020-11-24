e18cd35430ad9cc86c2a87ed8d408f99bec52161
Andrey Tikhonov <andrey.tikhonov.1974@gmail.com>
2020-11-24 17:49:18 +0200

    Docker

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo docker run -d  --name yahoo_parser  parser:yahoo  -c $1 -o ./
+sudo docker run -d  --name yahoo_parser  --net=host -e DBHOST='localhost' -e DBPORT=3306 -e DATABASE='traxessag' -e DBUSER='admin' -e DBPASSWORD='Qwerty@123' parser:yahoo  -c PD -o ./
 
